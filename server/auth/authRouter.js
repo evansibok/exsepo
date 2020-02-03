75eab50535a4ddd609383c1fb0e302bb04a84c76
Evans Ibok <evansibok@gmail.com>
2020-02-03 10:19:27 +0100

    Import loginUser to authRouter file

--- server/auth/authRouter.js
+++ server/auth/authRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const validateUser = require('../middlewares/validateUser');
 const registerUser = require('./registerUser');
+const loginUser = require('./loginUser');
 
 router.post('/register', validateUser, registerUser);
 router.post('/login', validateUser, loginUser);
